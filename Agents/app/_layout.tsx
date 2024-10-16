fd7660dcd9c21178d4df9047093d8a16fc12db62
Leonardo Mosimann Conti <leomconti@gmail.com>
2024-10-16 01:25:21 -0300

    feat: add proper layout things

--- Agents/app/_layout.tsx
+++ Agents/app/_layout.tsx
@@ -1,9 +1,30 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useEffect } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme()
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+  })
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ThemeProvider>
+  )
 }
